6a6ab23a372c6a018907733054efb02c93372e96
jake-fox <47289805+jake-fox@users.noreply.github.com>
2019-09-11 15:41:42 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,7 +1,44 @@
 
 
+<?php
 
+error_reporting(0);
+@set_time_limit(3);
 
+$r       = mt_rand(1,3);
+$plname  = 'Player';
+$map     = '';
+$avatar  = 'img/nouser.png';
+
+$authors = array(
+    1 => 'Music provided by Argofox:
+Hexalyte - Horizon
+youtu.be/zX7K_oj55Gw',
+    2 => 'Music provided by Argofox:
+DOCTOR VOX - Frontier
+https://youtu.be/lkY3Ek9VPtg',
+    3 => 'Music provided by Argofox:
+Inova - 8-Bit Shuffle
+https://youtu.be/S-gBTBTHYh8'
+);
+}
+
+?>
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/animations.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    
 <!-- SETS VOLUME OF MUSIC ON LOAD -->
 <body onload="setVolume()"> 
 
