ee0bd4f8b7d208b6c2b54ac3ba36c43a514f328b
jake-fox <47289805+jake-fox@users.noreply.github.com>
2019-09-11 15:39:57 +0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,11 @@
+
+
+
+
+<!-- SETS VOLUME OF MUSIC ON LOAD -->
+<body onload="setVolume()"> 
+
+<video id="myVideo" width="0" height="0" controls style="opacity: 0">
+  <source src="music/<?php echo $r?>.ogg" type="audio/ogg">
+  
+</video>
